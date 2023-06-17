61a558b1e42f9ca96fc5e9f8d2373ed8fa8d4c67
JGoode2009 <105678312+JGoode2009@users.noreply.github.com>
2023-06-16 23:00:50 -0500

    Create song_dictionary_example.py
    example output from the api

---
+++ song_dictionary_example.py
@@ -0,0 +1,96 @@
+songs = {'album': {'album_type': 'album', 'artists': [{'external_urls': {'spotify': 'https://open.spotify.com/artist/0urTpYCsixqZwgNTkPJOJ4'}, 
+'href': 'https://api.spotify.com/v1/artists/0urTpYCsixqZwgNTkPJOJ4',
+'id': '0urTpYCsixqZwgNTkPJOJ4',
+'name': 'Aaliyah',
+'type': 'artist',
+'uri': 'spotify:artist:0urTpYCsixqZwgNTkPJOJ4'}],
+'external_urls': {'spotify': 'https://open.spotify.com/album/50GHwAog5gmnqkypVkDso8'},
+'href': 'https://api.spotify.com/v1/albums/50GHwAog5gmnqkypVkDso8',
+'id': '50GHwAog5gmnqkypVkDso8',
+'images': [{'height': 640, 'url': 'https://i.scdn.co/image/ab67616d0000b273cb8a7cad58c0450ec04529fb', 'width': 640}, {'height': 300, 'url': 'https://i.scdn.co/image/ab67616d00001e02cb8a7cad58c0450ec04529fb', 'width': 300}, {'height': 64, 'url': 'https://i.scdn.co/image/ab67616d00004851cb8a7cad58c0450ec04529fb', 'width': 64}], 'is_playable': True, 'name': 'I Care 4 U', 'release_date': '2002-12-10', 'release_date_precision': 'day', 'total_tracks': 16, 'type': 'album', 'uri': 'spotify:album:50GHwAog5gmnqkypVkDso8'},
+'artists': [{'external_urls': {'spotify': 'https://open.spotify.com/artist/0urTpYCsixqZwgNTkPJOJ4'}, 'href': 'https://api.spotify.com/v1/artists/0urTpYCsixqZwgNTkPJOJ4', 'id': '0urTpYCsixqZwgNTkPJOJ4', 'name': 'Aaliyah', 'type': 'artist', 'uri': 'spotify:artist:0urTpYCsixqZwgNTkPJOJ4'}],
+'disc_number': 1, 'duration_ms': 265666, 'explicit': False, 'external_ids': {'isrc': 'USUYG1352903'}, 'external_urls': {'spotify': 'https://open.spotify.com/track/3PqAnW2bB5TQnMoBivyFOb'}, 'href': 'https://api.spotify.com/v1/tracks/3PqAnW2bB5TQnMoBivyFOb', 'id': '3PqAnW2bB5TQnMoBivyFOb', 'is_local': False, 'is_playable': True, 'name': 'Are You That Somebody', 'popularity': 71, 'preview_url': 'https://p.scdn.co/mp3-preview/12ec6bd01076a1404520c70cb0595b0f6595d0f1?cid=a6ab315119394a7793d134824c63f107', 'track_number': 2, 'type': 'track', 'uri': 'spotify:track:3PqAnW2bB5TQnMoBivyFOb'}
+
+songs2 = {
+    "album": {
+        "album_type": "album",
+        "artists": [
+            {
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/artist/0urTpYCsixqZwgNTkPJOJ4"
+                },
+                "href": "https://api.spotify.com/v1/artists/0urTpYCsixqZwgNTkPJOJ4",
+                "id": "0urTpYCsixqZwgNTkPJOJ4",
+                "name": "Aaliyah",
+                "type": "artist",
+                "uri": "spotify:artist:0urTpYCsixqZwgNTkPJOJ4",
+            }
+        ],
+        "external_urls": {
+            "spotify": "https://open.spotify.com/album/50GHwAog5gmnqkypVkDso8"
+        },
+        "href": "https://api.spotify.com/v1/albums/50GHwAog5gmnqkypVkDso8",
+        "id": "50GHwAog5gmnqkypVkDso8",
+        "images": [
+            {
+                "height": 640,
+                "url": "https://i.scdn.co/image/ab67616d0000b273cb8a7cad58c0450ec04529fb",
+                "width": 640,
+            },
+            {
+                "height": 300,
+                "url": "https://i.scdn.co/image/ab67616d00001e02cb8a7cad58c0450ec04529fb",
+                "width": 300,
+            },
+            {
+                "height": 64,
+                "url": "https://i.scdn.co/image/ab67616d00004851cb8a7cad58c0450ec04529fb",
+                "width": 64,
+            },
+        ],
+        "is_playable": True,
+        "name": "I Care 4 U",
+        "release_date": "2002-12-10",
+        "release_date_precision": "day",
+        "total_tracks": 16,
+        "type": "album",
+        "uri": "spotify:album:50GHwAog5gmnqkypVkDso8",
+    },
+    "artists": [
+        {
+            "external_urls": {
+                "spotify": "https://open.spotify.com/artist/0urTpYCsixqZwgNTkPJOJ4"
+            },
+            "href": "https://api.spotify.com/v1/artists/0urTpYCsixqZwgNTkPJOJ4",
+            "id": "0urTpYCsixqZwgNTkPJOJ4",
+            "name": "Aaliyah",
+            "type": "artist",
+            "uri": "spotify:artist:0urTpYCsixqZwgNTkPJOJ4",
+        }
+    ],
+    "disc_number": 1,
+    "duration_ms": 265666,
+    "explicit": False,
+    "external_ids": {"isrc": "USUYG1352903"},
+    "external_urls": {
+        "spotify": "https://open.spotify.com/track/3PqAnW2bB5TQnMoBivyFOb"
+    },
+    "href": "https://api.spotify.com/v1/tracks/3PqAnW2bB5TQnMoBivyFOb",
+    "id": "3PqAnW2bB5TQnMoBivyFOb",
+    "is_local": False,
+    "is_playable": True,
+    "name": "Are You That Somebody",
+    "popularity": 71,
+    "preview_url": "https://p.scdn.co/mp3-preview/12ec6bd01076a1404520c70cb0595b0f6595d0f1?cid=a6ab315119394a7793d134824c63f107",
+    "track_number": 2,
+    "type": "track",
+    "uri": "spotify:track:3PqAnW2bB5TQnMoBivyFOb",
+}
+ 
+
+
+
+
+for key in songs2:
+    if key == 'name':
+        print(songs[key])
